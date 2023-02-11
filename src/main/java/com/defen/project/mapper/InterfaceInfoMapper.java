a0c57660c3937c6efbf7b7321e99e2d8f0e4b8dd
defeng <defeng@qq.com>
2023-02-11 22:37:01 +0800

    新增api接口相关代码

---
+++ src/main/java/com/defen/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.defen.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.defen.project.model.entity.InterfaceInfo;
+
+/**
+* @author defen
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-02-11 19:42:29
+* @Entity com.defen.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
