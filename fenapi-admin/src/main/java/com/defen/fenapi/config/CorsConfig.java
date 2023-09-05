6440430ae965c800d995a9c09ec135383cf38a61
DeFeng Chen <90104643+defen2001@users.noreply.github.com>
2023-09-06 00:00:59 +0800

    fix

--- fenapi-admin/src/main/java/com/defen/fenapi/config/CorsConfig.java
+++ fenapi-admin/src/main/java/com/defen/fenapi/config/CorsConfig.java
@@ -17,12 +17,13 @@ public class CorsConfig implements WebMvcConfigurer {
         // 覆盖所有请求
         registry.addMapping("/**")
                 // 放行哪些域名（必须用 patterns，否则 * 会和 allowCredentials 冲突）
-                .allowedOriginPatterns("*")
-//                .allowedOrigins("https://fenapi.cn","http://fenapi.cn")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
                 // 允许发送 Cookie
                 .allowCredentials(true)
-                .exposedHeaders("Authorization");
+                // .allowedOriginPatterns("*")
+                .allowedOrigins("http://fenapi.cn")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .exposedHeaders("*");
     }
 }
