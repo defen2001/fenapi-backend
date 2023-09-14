865f3a26cd2a6ea9edbcab340c0cbe65f7500999
defeng <defeng@qq.com>
2023-09-14 15:49:13 +0800

    reactor: 线上部署修改

--- fenapi-admin/src/main/java/com/defen/fenapi/FenapiAdminApplication.java
+++ fenapi-admin/src/main/java/com/defen/fenapi/FenapiAdminApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.defen.fenapi.mapper")
-//@EnableDubbo
+@EnableDubbo
 public class FenapiAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(FenapiAdminApplication.class, args);
